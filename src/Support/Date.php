4bf8cfdde43bd53b3377988e4330deaf9a08c1cf
hengcs <hengcs@gmail.com>
2015-12-03 00:17:52 +0800

    added Date helper

--- src/Support/Date.php
+++ src/Support/Date.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: hengc
- * Date: 12/2/2015
- * Time: 11:04 PM
+ * Laravel Commons
+ *
+ * @author    Iain Heng <hengcs@gmail.com>
+ * @copyright 2015 Heng Cheng Siang
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://github.com/hengcs/laravel-commons
  */
 
 namespace Iainh\LaravelCommons;
@@ -11,5 +13,19 @@ namespace Iainh\LaravelCommons;
 
 class Date
 {
+    /**
+     * Print a date string by specifying a format
+     *
+     * @param string $string_date
+     * @param string $format
+     * @return bool|string
+     */
+    public static function format($string_date = null, $format = null)
+    {
+        if(!$string_date) $string_date = date('Y-m-d');
 
+        if(!$format) $format = 'd/m/Y';
+
+        return date($format, strtotime($string_date));
+    }
 }
